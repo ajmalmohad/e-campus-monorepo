74e8257952448d4ab1616697444a84d8e05a30d3
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-11-04 13:29:12 +0530

    Job Cards

--- org-frontend/src/pages/OrganizationDashboard.jsx
+++ org-frontend/src/pages/OrganizationDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Search from '../components/Search'
 import {Chip} from "@nextui-org/react";
+import JobCard from '../components/JobCard';
 
 function OrganizationDashboard() {
   return (
     <div>
       <div>
         <Search className="w-full"/>
+
         <div className='filters py-4 flex gap-2'>
         <Chip color="warning" variant="solid" className='text-white cursor-pointer'>New</Chip>
         <Chip color="warning" variant="bordered" className='cursor-pointer'>A-Z</Chip>
@@ -14,6 +16,17 @@ function OrganizationDashboard() {
         <Chip color="warning" variant="bordered" className='cursor-pointer'>Closed</Chip>
         <Chip color="warning" variant="bordered" className='cursor-pointer'>Draft</Chip>
         </div>
+
+        <div className="gap-4 grid grid-cols-4 sm:grid-cols-4">
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+        </div>
+
       </div>
     </div>
   )
