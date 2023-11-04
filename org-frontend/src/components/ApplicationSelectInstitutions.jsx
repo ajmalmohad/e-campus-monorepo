28619253c846427be7683a53270afc589ec845a6
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-11-04 20:18:33 +0530

    Application Forms

--- org-frontend/src/components/ApplicationSelectInstitutions.jsx
+++ org-frontend/src/components/ApplicationSelectInstitutions.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
 import {Button} from '@nextui-org/react'
 import { Divider } from '@mui/material'
+import {CheckboxGroup} from "@nextui-org/react";
+import {CustomCheckbox} from "./CustomCheckBox.jsx";
 
-function ApplicationSelectInstitutions({ submit, prev }) {
+
+function ApplicationSelectInstitutions({ submit, prev, groupSelected, setGroupSelected, institutions }) {
+
+    console.log(institutions);
   return (
     <div>
         <br/><h1 className='text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight'>Select Institutions</h1>
         <br/><Divider />
-        <Button onClick={prev}>Prev</Button>
+        <div className='py-2 px-4'>
+            <div className="py-4 flex flex-col gap-1 w-full">
+                <CheckboxGroup
+                    label="Select Institutions"
+                    value={groupSelected}
+                    onChange={setGroupSelected}
+                    classNames={{
+                    base: "w-full"
+                    }}
+                >
+                    {
+                        institutions.map((item) => {
+                            return <CustomCheckbox
+                                value={item}
+                                user={{
+                                    name: item,
+                                    username: item.toLowerCase().split(" ").join(""),
+                                    status: "Tier 2",
+                                }}
+                                statusColor="secondary"
+                            />
+                        })
+                    }
+                    
+                </CheckboxGroup>
+            </div>
+        
+            <div className='flex gap-4'>
+                <Button onClick={prev}>Prev</Button>
+                <Button color='success' onClick={submit}>Submit</Button>
+            </div>
+        </div>
     </div>
   )
 }
--- org-frontend/src/pages/CreateForm.jsx
+++ org-frontend/src/pages/CreateForm.jsx
@@ -7,6 +7,8 @@ function CreateForm() {
   
   let [form, setForm] = useState(1);
   let [fields, setFields] = useState(['Name', 'Email', 'Phone Number', 'CGPA']);
+  const [groupSelected, setGroupSelected] = useState([]);
+  const [allInstitutions, setInstitutions] = useState(["Jyothi Engineering College", "MEA Engineering College",  "Nehru Engineering College", "Al Ameen Engineering College", "Model Engineering College", "Government Engineering College"]);
 
   let clickNext = () => {
     setForm(prev => prev+1);
@@ -25,7 +27,7 @@ function CreateForm() {
       {
         form == 1 ? <ApplicationDetailsForm next={clickNext} /> : 
         form == 2 ? <ApplicationCreateForm next={clickNext} prev={clickPrev} fields={fields} setFields={setFields} /> :
-        form == 3 ? <ApplicationSelectInstitutions submit={clickSubmit} prev={clickPrev}/> :
+        form == 3 ? <ApplicationSelectInstitutions submit={clickSubmit} prev={clickPrev} groupSelected={groupSelected} setGroupSelected={setGroupSelected} institutions={allInstitutions}/> :
         <></>
       }
     </div>
